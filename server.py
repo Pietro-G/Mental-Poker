9eba53fe836edb5bf1045ea2c2f86cc672922b07
Alaska <lillywiz@gmail.com>
2019-05-11 20:10:54 -0600

    add shuffle and encrypt functions into player class

--- server.py
+++ server.py
@@ -10,6 +10,62 @@ DEFAULT_ADDR = "127.0.0.1"
 DEFAULT_PORT = 8123
 game = coordinator.Game()
 
+class Player:
+    def __init__(self, ip):
+        self.ip = ip
+    
+    def start(self):
+        _thread.start_new_thread(clientthread,(conn,addr))
+    
+    def finalize_deck(self):
+        raise NotImplementedError()
+
+    def notice_shuffle(self):
+        message_to_send = "<" + addr[0] + "> " + " has shuffled"
+        print(message_to_send)
+        broadcast(message_to_send, conn)
+    
+    def notice_encrypt(self):
+        message_to_send = "<" + addr[0] + "> " + " has encrypted"
+        print(message_to_send)
+        broadcast(message_to_send, conn)
+    
+    def shuffled(message):
+        shuffled_deck = message['shuffled_deck']
+        game.shuffle(addr, shuffled_deck)
+        message_to_send = "<" + addr[0] + "> " + " has the shuffled deck"
+        broadcast(message_to_send, conn)
+        print(message_to_send)
+        return shuffled_deck
+    
+    def encrypted(message):
+        encrypted_deck = message['encrypted_deck']
+        message_to_send = "<" + addr[0] + "> " + " has the encrypted deck"
+        broadcast(message_to_send, conn)
+        print(message_to_send)
+        return encrypted_deck
+    
+    def clientthread(conn, addr):
+        # sends a message to the client whose user object is conn
+        conn.send("Welcome to Blackjack!".encode())
+        try:
+            game.join(coordinator.Player(addr))
+        except:
+            conn.send("Failure to join. Disconnecting.".encode())
+            remove(conn)
+        
+        while True:
+            try:
+                message = conn.recv(2048)
+                json.loads(message)
+                if message['action'] == 'shuffled':
+                    shuffled(message)
+                elif message['action'] == 'encrypted':
+                    encrypted(message)
+            except:
+                continue
+
+
 def runServer():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -23,29 +79,6 @@ def runServer():
     
     list_of_clients = []
     
-    def clientthread(conn, addr):
-        # sends a message to the client whose user object is conn
-        conn.send("Welcome to Blackjack!".encode())
-        try:
-            game.join(coordinator.Player(addr))
-        except:
-            conn.send("Failure to join. Disconnecting.".encode())
-            remove(conn)
-    
-        while True:
-            try:
-                message = conn.recv(2048)
-                print("<" + addr[0] + ">: " + message.decode())
-                if message:
-                    print("<" + addr[0] + ">: " + message)
-                    #We need to make sure the server is sending the msg associated with the user's choice
-                    message_to_send = "<" + addr[0] + "> " + message
-                    game_move(addr, message)
-                    broadcast(message_to_send, conn)
-                else:
-                    remove(conn)
-            except:
-                continue
 
     def broadcast(message, connection):
         for clients in list_of_clients:
@@ -62,9 +95,6 @@ def runServer():
         if connection in list_of_clients:
             list_of_clients.remove(connection)
 
-    #Call the game object's next move
-    def game_move(addr, msg):
-        return game.recv_from_client(addr, msg)
 
     while True:
         
@@ -74,7 +104,8 @@ def runServer():
         # prints the address of the user that just connected
         print(addr[0] + " connected")
             # creates and individual thread for every user
-        _thread.start_new_thread(clientthread,(conn,addr))
-                
+        player = Player(addr)
+        player.start()
+    
     conn.close()
     server.close()
